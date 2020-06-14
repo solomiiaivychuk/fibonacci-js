6263c9fc4812839bd68c901beabfcb661b3e9d4c
solomiiaivychuk <52928387+solomiiaivychuk@users.noreply.github.com>
2020-06-14 14:22:39 +0300

    Milestone 6 in process
    Spinner for the previous results list is not finished yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,59 @@
 let button = document.querySelector(".btn");
 
 let fibonacci = document.getElementById("Y");
-let spinner = document.querySelector(".spinner-border");
-let dangerAlert = document.querySelector('.alert-danger');
+let calcSpinner = document.querySelector(".calc-spinner");
+let dangerAlert = document.querySelector(".alert-danger");
 let input = document.getElementById("X");
-let serverError = document.querySelector('.server-error');
+let serverError = document.querySelector(".server-error");
 
 button.addEventListener("click", function () {
   let number = document.getElementById("X").value;
   let address = `http://localhost:5050/fibonacci/${number}`;
 
   if (number > 50) {
-      dangerAlert.classList.remove("d-none");
-      input.classList.add("danger");
-    } 
-    else {
-        dangerAlert.classList.add("d-none");
-        input.classList.remove("danger");
-        spinner.classList.remove("d-none");
-        fetch(address)
-        .then((response) => response.json())
-        .then((data) => {
-        spinner.classList.add("d-none");
+    dangerAlert.classList.remove("d-none");
+    input.classList.add("danger");
+  } else {
+    dangerAlert.classList.add("d-none");
+    input.classList.remove("danger");
+    calcSpinner.classList.remove("d-none");
+    fetch(address)
+      .then((response) => response.json())
+      .then((data) => {
+        calcSpinner.classList.add("d-none");
         fibonacci.innerText = data.result;
+        getCalculationsList();
       })
       .catch((error) => {
         fibonacci.classList.add("d-none");
-        spinner.classList.add("d-none");
+        calcSpinner.classList.add("d-none");
         serverError.classList.remove("d-none");
         serverError.innerText = error;
-      });     
-}
+      });
+  }
 });
+
+function getCalculationsList() {
+  let prevResultsList = document.getElementById("prev-results-list");
+  let url = 'http://localhost:5050/getFibonacciResults';
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    data.results.sort((a, b) => b.createdDate - a.createdDate);
+    for (let i = 0; i < data.results.length; i++) {
+      let item = document.createElement('li');
+      let text = document.createTextNode(
+        "The Fibonacci of "+ data.results[i].number 
+        + " is " + data.results[i].result
+        + ". Calculated at: " + new Date(data.results[i].createdDate));
+      item.appendChild(text);
+      prevResultsList.appendChild(item);
+
+    }
+  });
+}
+//let body = document.getElementsByTagName("body");
+document.addEventListener("DOMContentLoaded", getCalculationsList);
+
+
