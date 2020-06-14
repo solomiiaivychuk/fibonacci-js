afaa1e573751831ad24a747bb7141545bcd5c8bb
solomiiaivychuk <52928387+solomiiaivychuk@users.noreply.github.com>
2020-06-14 15:39:39 +0300

    Milestone 6 in process.
    *Regarding fact that list of previous results is not resreshing - I've put the results in ordrered list to see if the number of items is changing. It is actually refreshing, but it adds another list to the end of previous one. When I open the page, it has 187 items, after calculation there is 385 elements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,79 @@
-let button = document.querySelector(".btn");
+(() => {
+  let button = document.querySelector(".btn");
 
-let fibonacci = document.getElementById("Y");
-let calcSpinner = document.querySelector(".calc-spinner");
-let dangerAlert = document.querySelector(".alert-danger");
-let input = document.getElementById("X");
-let serverError = document.querySelector(".server-error");
-
-button.addEventListener("click", function () {
-  let number = document.getElementById("X").value;
-  let address = `http://localhost:5050/fibonacci/${number}`;
-
-  if (number > 50) {
-    dangerAlert.classList.remove("d-none");
-    input.classList.add("danger");
-  } else {
-    dangerAlert.classList.add("d-none");
-    input.classList.remove("danger");
-    calcSpinner.classList.remove("d-none");
-    fetch(address)
-      .then((response) => response.json())
-      .then((data) => {
-        calcSpinner.classList.add("d-none");
-        fibonacci.innerText = data.result;
-        getCalculationsList();
-      })
-      .catch((error) => {
-        fibonacci.classList.add("d-none");
-        calcSpinner.classList.add("d-none");
-        serverError.classList.remove("d-none");
-        serverError.innerText = error;
-      });
-  }
-});
-
-function getCalculationsList() {
+  let fibonacci = document.getElementById("Y");
+  let calcSpinner = document.querySelector(".calc-spinner");
+  let dangerAlert = document.querySelector(".alert-danger");
+  let input = document.getElementById("X");
+  let serverError = document.querySelector(".server-error");
+  let resultsSpinner = document.querySelector(".results-spinner");
   let prevResultsList = document.getElementById("prev-results-list");
-  let url = 'http://localhost:5050/getFibonacciResults';
-  fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    data.results.sort((a, b) => b.createdDate - a.createdDate);
-    for (let i = 0; i < data.results.length; i++) {
-      let item = document.createElement('li');
-      let text = document.createTextNode(
-        "The Fibonacci of "+ data.results[i].number 
-        + " is " + data.results[i].result
-        + ". Calculated at: " + new Date(data.results[i].createdDate));
-      item.appendChild(text);
-      prevResultsList.appendChild(item);
 
+  button.addEventListener("click", function () {
+    let number = document.getElementById("X").value;
+    let address = `http://localhost:5050/fibonacci/${number}`;
+
+    if (number > 50) {
+      dangerAlert.classList.remove("d-none");
+      input.classList.add("danger");
+    } else {
+      dangerAlert.classList.add("d-none");
+      input.classList.remove("danger");
+      calcSpinner.classList.remove("d-none");
+      fetch(address)
+        .then((response) => response.json())
+        .then((data) => {
+          calcSpinner.classList.add("d-none");
+          resultsSpinner.classList.remove("d-none");
+          fibonacci.innerText = data.result;
+        })
+        .then(async () => {
+          await getCalculationsList();
+        })
+        .catch((error) => {
+          //handleError()
+          fibonacci.classList.add("d-none");
+          calcSpinner.classList.add("d-none");
+          serverError.classList.remove("d-none");
+          serverError.innerText = error;
+        });
     }
   });
-}
-//let body = document.getElementsByTagName("body");
-document.addEventListener("DOMContentLoaded", getCalculationsList);
 
+  async function getCalculationsList() {
+    let url = "http://localhost:5050/getFibonacciResults";
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        showDBresults(data);
+        resultsSpinner.classList.add("d-none");
+      });
+  }
 
+  function showDBresults(data) {
+    let sortedData = data.results.sort((a, b) => b.createdDate - a.createdDate);
+    console.log(sortedData);
+    for (let i = 0; i < sortedData.length; i++) {
+      let item = document.createElement("li");
+      let text = document.createTextNode(
+        "The Fibonacci of " +
+          sortedData[i].number +
+          " is " +
+          sortedData[i].result +
+          ". Calculated at: " +
+          new Date(sortedData[i].createdDate)
+      );
+      item.appendChild(text);
+      prevResultsList.appendChild(item);
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    resultsSpinner.classList.remove("d-none");
+    getCalculationsList();
+    prevResultsList.innerHTML.reload;
+  });
+
+  function sortList(array) {}
+})();
